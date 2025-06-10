0b87dba662e649393a8748758698be727edb782f
FranciscaCMG <francisca.marquez@usach.cl>
2025-06-09 20:12:39 -0400

    FIX login

--- resultado-final/js/login.js
+++ resultado-final/js/login.js
@@ -1,31 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("loginForm");
 
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
+  fetch('../data/users.json')
+    .then((res) => res.json())
+    .then((data) => {
+      form.addEventListener("submit", function (e) {
+        e.preventDefault();
 
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
 
-    fetch("http://localhost:3001/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ email, password })
-    })
-      .then((res) => {
-        if (res.status === 200) {
-          localStorage.setItem("auth", "true"); // <- Aquí
-          alert("✅ Login exitoso. Redirigiendo... hols");
+        const user = data.find((u) => u.email === email && u.password === password);
+        if (user) {
+          localStorage.setItem("auth", "true");
+          alert("✅ Login exitoso. Redirigiendo...");
           window.location.href = "index.html";
         } else {
           alert("❌ Credenciales incorrectas");
         }
-      })
-      .catch((error) => {
-        console.error("Error al conectar con el servidor:", error);
-        alert("Ocurrió un error de conexión.");
       });
-  });
+    })
+    .catch((error) => {
+      console.error("Error al cargar el archivo JSON:", error);
+    });
 });
